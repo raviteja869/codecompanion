987ed064c1f0b8c37906d0c15ca8e862e84ebe15
raviteja869 <132315611+raviteja869@users.noreply.github.com>
2023-05-21 19:39:18 -0400

    Update solution_generator.py

--- solution_generation/solution_generator.py
+++ solution_generation/solution_generator.py
@@ -1,13 +1,25 @@
 import openai
+from pylint import epylint as lint
+from bugspots import Bugspots
 
 openai.api_key = 'sk-APIKEY'
 
 def generate_solution(code, problem):
-    prompt = f"{code}\n# Problem: {problem}\n# Solution:"
+    # Analyze the code for errors and code smells
+    pylint_stdout, pylint_stderr = lint.py_run(code, return_std=True)
+    pylint_output = pylint_stdout.getvalue()
+
+    # Predict potential bugs in the code
+    bugspots = Bugspots()
+    bug_prediction = bugspots.get_hotspots(code)
+
+    # Create a prompt for GPT-3 that includes the code, problem description, pylint output, and bug prediction
+    prompt = f"{code}\n# Problem: {problem}\n# Pylint Output: {pylint_output}\n# Bug Prediction: {bug_prediction}\n# Solution:"
+    
     response = openai.Completion.create(
-      engine="text-davinci-003",
-      prompt=prompt,
-      temperature=0.5,
-      max_tokens=100
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=100
     )
     return response.choices[0].text.strip()

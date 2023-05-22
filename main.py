01182f0d53d85ca0d702531111005ae03908bdeb
raviteja869 <132315611+raviteja869@users.noreply.github.com>
2023-05-22 10:08:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-
 from input_processing.input_processor import process_input
 from code_analysis.code_analyzer import analyze_code
 from solution_generation.solution_generator import generate_solution
 from output_processing.output_processor import process_output
 
 def main():
-    processed_input = process_input()
-    print(processed_input)
-    processed_input = process_input()
-    analysis_result = analyze_code(processed_input)
-    solution = generate_solution(analysis_result)
-    output = process_output(solution)
-    print(output)
+    try:
+        processed_input = process_input()
+        analysis_result = analyze_code(processed_input)
+        solution = generate_solution(analysis_result)
+        output = process_output(solution)
+        print(output)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 if __name__ == "__main__":
     main()
-
-
-
-
-

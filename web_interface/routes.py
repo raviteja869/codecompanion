e1433f0af1078a7375ce23341968eef96351dd92
raviteja869 <132315611+raviteja869@users.noreply.github.com>
2023-05-22 10:07:36 -0400

    Update routes.py

--- web_interface/routes.py
+++ web_interface/routes.py
@@ -1,13 +1,15 @@
-from flask import request, render_template, redirect, url_for
-from flask_login import login_user, logout_user, login_required
-from werkzeug.security import check_password_hash
+from flask import request, render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
+from itsdangerous import URLSafeTimedSerializer
 
-from web_interface import app
+from web_interface import app, mail
 from web_interface.user_management import load_user, User
 from input_processing.input_processor import process_input
 from code_analysis.code_analyzer import analyze_code
 from solution_generation.solution_generator import generate_solution
 from output_processing.output_processor import process_output
+from flask_mail import Message
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -45,3 +47,55 @@ def index():
         except Exception as e:
             output = "An unexpected error occurred."
     return render_template('index.html', output=output)
+@app.route('/reset-password', methods=['GET', 'POST'])
+def reset_password():
+    if request.method == 'POST':
+        email = request.form['email']
+        user = User.query.filter_by(email=email).first()
+        if user:
+            token = URLSafeTimedSerializer(app.config['SECRET_KEY']).dumps(email, salt='password-reset-salt')
+            msg = Message('Password Reset Request', sender='noreply@codecompanion.com', recipients=[email])
+            msg.body = f'Click the following link to reset your password: {url_for('reset_password_token', token=token, _external=True)}'
+            mail.send(msg)
+            flash('A password reset link has been sent to your email.', 'info')
+        else:
+            flash('No account found with that email.', 'warning')
+    return render_template('reset_password_request.html')
+
+@app.route('/reset-password/<token>', methods=['GET', 'POST'])
+def reset_password_token(token):
+    try:
+        email = URLSafeTimedSerializer(app.config['SECRET_KEY']).loads(token, salt='password-reset-salt', max_age=3600)
+    except:
+        flash('The password reset link is invalid or has expired.', 'warning')
+        return redirect(url_for('reset_password'))
+    user = User.query.filter_by(email=email).first()
+    if request.method == 'POST':
+        password = request.formI apologize for the cut-off in the previous message. Here's the continuation of the code:
+
+```python
+        password_hash = generate_password_hash(password)
+        user.password_hash = password_hash
+        db.session.commit()
+        flash('Your password has been reset.', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_password_token.html')
+
+@app.route('/verify-email/<token>', methods=['GET'])
+def verify_email(token):
+    try:
+        email = URLSafeTimedSerializer(app.config['SECRET_KEY']).loads(token, salt='email-verification-salt', max_age=3600)
+    except:
+        flash('The email verification link is invalid or has expired.', 'warning')
+        return redirect(url_for('index'))
+    user = User.query.filter_by(email=email).first()
+    user.email_verified = True
+    db.session.commit()
+    flash('Your email has been verified.', 'success')
+    return redirect(url_for('index'))
+
+@app.route('/history', methods=['GET'])
+@login_required
+def history():
+    submissions = Submission.query.filter_by(user_id=current_user.id).all()
+    return render_template('history.html', submissions=submissions)
